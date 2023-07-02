6656f1a30ba2064492cd24ed1e980ca398a36695
Sidharth Mohanty <sidmohanty11@gmail.com>
2023-07-03 04:05:53 +0530

    ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sidmohanty11.github.io',
   integrations: [tailwind()]
 });
